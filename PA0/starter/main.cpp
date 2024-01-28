51caa7451e5141b2f68bd238a57be355d463e0aa
Venkat Nallam <nallamsaiv@gmail.com>
2024-01-28 14:59:44 -0600

    change - 2

---
+++ PA0/starter/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+ 
+template <class T> class SmartPointer {
+private:
+    T* ptr;
+public:
+    explicit SmartPointer(T* p = NULL) { 
+        ptr = p; 
+    }
+
+    T& operator*() { return *ptr; }
+
+    T* operator->() { return ptr; }
+
+    ~SmartPointer(){
+        if(ptr != nullptr){
+        delete ptr;
+    }
+        ptr = nullptr;
+    }
+ 
+};
+
+#ifndef TEST
+void memoryLeakCheck()
+{
+    SmartPointer<int> p1(new int(10000));
+
+    *p1 = 2000;
+    return;
+}
+
+
+int main()
+{   
+   memoryLeakCheck();
+   return 0;
+}
+#endif
